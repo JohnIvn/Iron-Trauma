958eba7050893de0b51ad058cab0a2ef23593b76
Ivan <janivantot@gmail.com>
2025-01-21 18:05:56 +0800

    fixed movement slide, converted to gdscript

---
+++ Scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const Speed = 300.0
+
+var _animationPlayer: AnimationPlayer
+
+func _ready():
+	_animationPlayer = $WalkingAnimation
+
+func _physics_process(_delta):
+	velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		velocity.x += Speed
+		if _animationPlayer:
+			_animationPlayer.play("walking_right")
+	elif Input.is_action_pressed("move_left"):
+		velocity.x -= Speed
+		if _animationPlayer:
+			_animationPlayer.play("walking_left")
+			
+	if velocity == Vector2.ZERO and _animationPlayer:
+		_animationPlayer.stop()
+
+	move_and_slide()
