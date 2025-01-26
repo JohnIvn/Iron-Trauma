a018b133d25285e343162ea348fe328e27743a72
Matthew Cania <matthewgab24@gmail.com>
2025-01-26 12:11:19 +0800

    Changes in demo
    Added some environmental effects in demo, updated the character textures

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,72 +1,84 @@
 extends CharacterBody2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var staminaBar: ProgressBar = $CanvasLayer/Control/ProgressBar
+# Nodes
+@onready var PlayerSprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var StaminaBar: ProgressBar = $CanvasLayer/Control/ProgressBar
 
-@export var ACCEL = 1.0
+# Constants
 
-@export var sprintSpeed = 300.0
-@export var walkSpeed = 100.0
+# Variables
+# Speed
+@export var acceleration = 10.0
+@export var sprintSpeed = 150.0
+@export var walkSpeed = 75.0
 var baseSpeed = 0.0
 
+# Health
 @export var maxStamina = 100.0
-@export var depletionRate = 50.0
+@export var depletionRate = 100.0
 @export var recoveryRate = 35.5
 var baseStamina = 0.0
 
+# Mechanics Variables
+# Movement
+var isMoving = false
+var isSprint = false
+var staminaInterval = 5.0
+var staminaCooldown
+
+# Vectors
 var input: Vector2
 
 func _ready() -> void:
 	baseStamina = maxStamina
+
+func _process(delta: float) -> void:
+# Animation Handler
+	_animation_handler()
+# Movement Handler
+	_movement_handler(delta)
 
 func _get_input():
 	input.x = Input.get_action_strength("move_right") - Input.get_action_strength('move_left')
 	input.y = Input.get_action_strength("move_down") - Input.get_action_strength('move_up')
 	
 	return input.normalized()
-	
-var isSprint = false
-var staminaInterval = 5.0
-var staminaCooldown
-	
-func _process(delta: float) -> void:
-	
-	staminaBar.value = baseStamina
-	_animation_handler()
+
+func _movement_handler(delta):
 	var playerInput = _get_input()
-	
+	StaminaBar.value = baseStamina
+
+# General Movement
+	velocity = lerp(velocity, playerInput * baseSpeed, delta * acceleration)
+	move_and_slide()
+
+# Sprint Mechanism
 	if Input.is_action_pressed('sprint') and baseStamina > 0 and isMoving:
 		isSprint = true
+	else:
+		isSprint = false
+
+	if isSprint:
 		baseSpeed = sprintSpeed
-		animated_sprite_2d.speed_scale = 2.0
 		baseStamina -= depletionRate * delta
 		baseStamina = clamp(baseStamina, 0, maxStamina)
 		if baseStamina <= 0:
 			isSprint = false
 			staminaCooldown = true
-	
 	else:
-		isSprint = false
-		animated_sprite_2d.speed_scale = 1.0
 		baseSpeed = walkSpeed
-		
-	if !isSprint:
+
 		if staminaCooldown:
+			PlayerSprite.speed_scale = 0.5
+			baseSpeed -= baseSpeed * 0.75
 			staminaInterval -= delta 
 			if staminaInterval <= 0:
 				staminaCooldown = false 
 				staminaInterval = 3.0 
-
 		else:
+			PlayerSprite.speed_scale = 1.0
+			baseSpeed = walkSpeed
 			baseStamina += recoveryRate * delta
 			baseStamina = clamp(baseStamina, 0, maxStamina)
-			
-	velocity = lerp(velocity, playerInput * baseSpeed, delta * ACCEL)
-	
-	move_and_slide()
-
-var isMoving = false
-var isLevt = false
 
 func _animation_handler():
 	if input.x != 0 or input.y != 0:
@@ -75,21 +87,15 @@ func _animation_handler():
 		isMoving = false
 	
 	if Input.is_action_pressed("move_left"):
-		animated_sprite_2d.flip_h = true
-		#sprite_2d.flip_h = true
+		PlayerSprite.flip_h = true
 	if Input.is_action_pressed("move_right"):
-		animated_sprite_2d.flip_h = false
-		#sprite_2d.flip_h = false
+		PlayerSprite.flip_h = false
 	
-	if isMoving:
-		#sprite_2d.visible = false
-		#animated_sprite_2d.visible = true
-		animated_sprite_2d.play('default')
-	else:
-		animated_sprite_2d.play('idle')
-		#sprite_2d.visible = true
-		#animated_sprite_2d.visible = false
+	if isMoving and !isSprint:
+		PlayerSprite.play('walk')
+	if isSprint and isMoving:
+		PlayerSprite.play('run')
+	if !isMoving and !isSprint:
+		PlayerSprite.play('idle')
 
-func drain():
-	baseSpeed = 0.0
 		
